01398dd54d85e3fbe4f15badd11fbc82f1a1edee
AsanbaevBaitemir <baitemir1706@gmail.com>
2024-05-19 12:28:14 +0600

    app init

---
+++ src/app.module.ts
@@ -0,0 +1,43 @@
+import { Module } from '@nestjs/common';
+
+import {join} from 'path';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { ConfigModule } from '@nestjs/config';
+import { User } from './users/user.model';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
+import { Post } from './posts/post.model';
+import { FilesModule } from './files/files.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { WalletModule } from './wallet/wallet.module';
+import { Wallet } from './wallet/wallet.model';
+
+@Module({
+  imports:[
+    ConfigModule.forRoot({
+       envFilePath:`.${process.env.NODE_ENV}.env`
+    }),
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      models: [User, Post, Wallet],
+      autoLoadModels:true,
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, 'static'),
+    }),
+    UsersModule,
+    AuthModule,
+    PostsModule,
+    FilesModule,
+    WalletModule,
+  ]
+})
+export class AppModule{
+
+}
